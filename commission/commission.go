3c21b2231f9ac929b0f3ddf13485065bb1215771
fara <iskfaran@gmail.com>
2026-04-18 19:46:49 +0500

    feat: расчёт комиссии

--- commission/commission.go
+++ commission/commission.go
@@ -1,1 +1,25 @@
 package commission
+
+import (
+	"fmt"
+	"math"
+)
+
+func Calculate(amount float64, isAlif bool) (float64, float64, error) {
+	if amount < 500 {
+		return 0, 0, fmt.Errorf("сумма слишком мала")
+	}
+	if amount > 15000000 {
+		return 0, 0, fmt.Errorf("превышен лимит ")
+	}
+
+	var fee float64
+	if isAlif {
+		fee = 0
+	} else {
+		fee = math.Round(amount * 0.0029)
+	}
+
+	total := amount + fee
+	return fee, total, nil
+}
